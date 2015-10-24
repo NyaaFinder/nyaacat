0ec4256c31936acfb967bfa5e7fa528583ca800f
zhengchun <zhengchun@souche.com>
2015-10-24 14:38:19 +0800

    update flux

---
+++ f2e/src/js/mobile.js
@@ -0,0 +1,23 @@
+/**
+ * Created by john on 15/10/19.
+ */
+import React from 'react';
+import Demo from './compoents/demo/demo.jsx';
+import {RootInstanceProvider} from 'react-hot-loader/Injection';
+
+var hotObj = React.render(
+    <div className="time">
+        <Demo name="入口2-移动端"/>
+    </div>,
+    document.getElementById('app')
+);
+
+// 生产环境注释掉
+if(module.hot){
+    RootInstanceProvider.injectProvider({
+        getRootInstances:function(){
+            return [hotObj];
+        }
+    });
+}
+
