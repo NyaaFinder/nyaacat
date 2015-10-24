4065b54d9f531dbfc8b3217bf359f2447ba2124a
XadillaX <admin@xcoder.in>
2015-10-24 15:08:30 +0800

    demo controller

--- nyaa.js
+++ nyaa.js
@@ -9,21 +9,28 @@
 global.__DOC_PATH = __dirname;
 require("app-module-path").addPath(__dirname + "/src");
 
+require("sugar");
+
 var config = require("config");
 var express = require("express");
 var 色 = require("colors");
 
-var nyaa = express();
+var nyaa = global.nyaa = express();
 
 var PORT = config.get("server.port");
 
 // some nes...ry modules
+require("lib/util");
 require("lib/toshihiko");
 
+// load static files
 nyaa.use(express.static(__dirname + "/f2e/build/", {
     dotfiles: "deny",
     lastModified: true
 }));
 
+// load routers
+require("lib/controller_loader")("");
+
 nyaa.listen(PORT);
-console.log(色.rainbow("Yuuka listened on port " + PORT + "."));
+console.log(色.rainbow("Nyaa listened on port " + PORT + "."));
