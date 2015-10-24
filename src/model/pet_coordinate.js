ad2cf0d71fa18fe10d6899476ab95f6a3bf7c7b7
XadillaX <admin@xcoder.in>
2015-10-24 21:48:06 +0800

    finished calculate coordinate algorithm

--- src/model/pet_coordinate.js
+++ src/model/pet_coordinate.js
@@ -1,12 +1,23 @@
 var toshihiko = require("lib/toshihiko");
+var util = require("util");
+var coordinator = require("lib/coodinator");
 
-var petCoordinate = toshihiko.define("pets_coordinate", [
-    { name: "id", 			column: "id", 			type: toshihiko.TYPE.Integer, primaryKey: true },
-    { name: "pet_id", 		column: "pet_id", 		type: toshihiko.TYPE.Integer },
-    { name: 'timestamp', 	column: 'timestamp', 	type: toshihiko.TYPE.Integer },
-    { name: 'lng',			column: 'lng', 			type: toshihiko.TYPE.Float },
-    { name: 'lat',			column: 'lat',			type: toshihiko.TYPE.Float }
+var PetCoordinate = toshihiko.define("pets_coordinate", [
+    { name: "id", column: "id", type: toshihiko.TYPE.Integer, primaryKey: true },
+    { name: "pet_id", column: "pet_id", type: toshihiko.TYPE.Integer },
+    { name: "timestamp", column: "timestamp", type: toshihiko.TYPE.Integer },
+    { name: "lng", column: "lng", type: toshihiko.TYPE.Float },
+    { name: "lat", column: "lat", type: toshihiko.TYPE.Float }
 ]);
 
-module.exports = petCoordinate;
+PetCoordinate.calculateCoordinate = function(petId, timestamp, callback) {
+    var PetPosition = util.getModel("PetPosition");
+    PetPosition.where({
+        "pet_id": petId,
+        timestamp: timestamp
+    }).find(function(err, positions) {
+        if(err) return callback(err);
+    });
+};
 
+module.exports = PetCoordinate;
